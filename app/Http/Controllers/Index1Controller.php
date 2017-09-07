71dabce04ab41f13f1ddd2cc8820c0eb2b050192
pardeepatgit <pardeep@catpl.co.in>
2017-09-07 17:34:21 +0530

    sad

---
+++ app/Http/Controllers/Index1Controller.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
+use App\Post;
+
+class Index1Controller extends Controller
+{
+    public function index(){
+        echo "HI====";
+    }
+}
